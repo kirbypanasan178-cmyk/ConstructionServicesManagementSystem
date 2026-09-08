505bead57330cd1da564a1d8df3ab4068f30f555
kirby john panasan <kirbypanasan178@outlook.com>
2026-09-08 10:58:25 +0800

    feat: add sidebar navigation

--- ConstructionServicesManagementSystem/Program.cs
+++ ConstructionServicesManagementSystem/Program.cs
@@ -37,6 +37,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Schedule}/{action=Weekly}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
